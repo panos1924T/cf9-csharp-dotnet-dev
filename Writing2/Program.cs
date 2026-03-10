8b7a7755cb7f8e8f3f271e725ad66f6d19393b39
PanTs <pants.ath@gmail.com>
2026-03-10 18:56:01 +0200

    feat: add writing 3 app

--- Writing2/Program.cs
+++ Writing2/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int num = 10;
+
+            // 1. Concatenation (old way)
+            Console.WriteLine("Num = " + num);
+
+            // 2. Composite formatting
+            Console.WriteLine("num = {0}", num);
+
+            // 3. String interpolation (new and prefered way)
+            Console.WriteLine($"num = {num}");
+
         }
     }
 }
---
+++ Writing3/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Writing3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            int b = 2;
+            int result = a + b;
+
+            // 1. Concatenation 
+            Console.WriteLine("The result of " + a + " and " + b + " is: " + result);
+
+            // 2. Composite formatting
+            Console.WriteLine("The result of {0} and {1} is: {2}", a, b, result);
+
+            // 3. String interpolation
+            Console.WriteLine($"The result of {a} and {b} is: {result}");
+        }
+    }
+}
