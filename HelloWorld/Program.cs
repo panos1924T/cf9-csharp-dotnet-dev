af57bed2bc5b5bf7dd355b4496f4cb0472923e74
PanTs <pants.ath@gmail.com>
2026-03-10 18:08:44 +0200

    initial commit

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World by coding factory!");
+            Console.WriteLine();
+        }
+    }
+}
