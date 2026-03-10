d852e13001acc4a1a49ae69300f404a346092426
PanTs <pants.ath@gmail.com>
2026-03-10 19:15:25 +0200

    feat: add validation with try parse

---
+++ NumberReadValidation/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace NumberReadValidation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float floatNum = 0F;
+            double doubleNum = 0D;
+
+            Console.WriteLine("Please, give two decimals");
+
+            if (!float.TryParse(Console.ReadLine(), out floatNum))
+                Console.WriteLine("Invalid number for a float number!");
+
+            if(!double.TryParse(Console.ReadLine(), out doubleNum))
+                Console.WriteLine("Invalid number for a double number!");
+
+            Console.WriteLine($"f = {floatNum:N2} \t d = {doubleNum:N2}");
+        }
+    }
+}
