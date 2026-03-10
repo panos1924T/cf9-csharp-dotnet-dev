523dab966af61b1c87322ca0c907475217e3c2b8
PanTs <pants.ath@gmail.com>
2026-03-10 19:06:53 +0200

    feat: add culture info demo app

---
+++ CultureInfoDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+
+namespace CultureInfoDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime d = new DateTime(2024, 6, 1);
+
+            Thread.CurrentThread.CurrentCulture = CultureInfo.GetCultureInfo("en-US");
+            Console.WriteLine("{0:N}", 1234.56);
+            Console.WriteLine("{0:D}", d);
+
+            Thread.CurrentThread.CurrentCulture = CultureInfo.GetCultureInfo("el-GR");
+            Console.WriteLine("{0:N}", 1234.56);
+            Console.WriteLine("{0:D}", d);
+            
+        }
+    }
+}
