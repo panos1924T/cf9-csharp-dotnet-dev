1122484c9b086819a8c39508d8ad3aa222e9474f
PanTs <pants.ath@gmail.com>
2026-03-10 18:19:10 +0200

    feat: add printing app

---
+++ Printing/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Printing
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 0;
+
+            Console.WriteLine("Num = {0}", num);
+            Console.WriteLine($"Num = {num}");
+        }
+    }
+}
